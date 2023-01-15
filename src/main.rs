87bdafed79ce05e901056abe666af9b1cfd44aec
a-isaiahharvey <41553768+a-isaiahharvey@users.noreply.github.com>
2023-01-15 07:37:58 -0400

    Implement localStorage

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,62 @@
 use leptos::*;
 
+fn get_stored_count() -> Option<usize> {
+    let storage = window()
+        .local_storage()
+        .expect("Could not find storage")
+        .expect("Could not get storage struct");
+
+    storage
+        .get("count")
+        .expect("Could not get `count` from storage")
+        .map(|s| s.parse::<usize>().expect("Could not parse string to usize"))
+}
+
+fn set_stored_count(value: usize) {
+    let storage = window()
+        .local_storage()
+        .expect("Could not find storage")
+        .expect("Could not get storage struct");
+
+    storage
+        .set("count", value.to_string().as_str())
+        .expect("Could not set `count` to storage")
+}
+
 #[component]
 fn SimpleCounter(cx: Scope) -> impl IntoView {
-    let (counter, set_counter) = create_signal(cx, 0);
+    let (count, set_count) = create_signal(cx, get_stored_count().unwrap_or_default());
 
-    let decrement = move |_| set_counter(counter() - 1);
+    window_event_listener("storage", move |_| {
+        if let Some(stored_count) = get_stored_count() {
+            if stored_count != count() {
+                set_stored_count(stored_count);
+                set_count(stored_count)
+            }
+        }
+    });
 
-    let increment = move |_| set_counter(counter() + 1);
+    let decrement = move |_| {
+        let result = count() - 1;
+        set_stored_count(result);
+        set_count(result);
+    };
+
+    let increment = move |_| {
+        let result = count() + 1;
+        set_stored_count(result);
+        set_count(result);
+    };
 
     view! { cx,
         <div class={"flex h-screen flex-col justify-center"}>
             <div id="count-layout">
                 {
-                    move || view! {cx, <div class={"text-slate-900 dark:text-white text-center text-9xl"}>{counter()}</div>}
+                    move || view! {cx, <div class={"text-slate-900 dark:text-white text-center text-9xl"}>{count()}</div>}
                 }
             </div>
             <div class={"add-subtract-buttons"}>
-                    {move || if counter() == 0 {
+                    {move || if count() == 0 {
                         view!{cx, <div></div>}.into_any()
 
                     } else {

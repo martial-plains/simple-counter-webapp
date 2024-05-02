c6e41ad580587b6c64d46550cf5cb6716f6bdeeb
Allister Isaiah Harvey <41553768+martial-plains@users.noreply.github.com>
2024-05-02 17:34:05 +0000

    Update leptos

--- src/main.rs
+++ src/main.rs
@@ -24,43 +24,43 @@ fn set_stored_count(value: usize) {
 }
 
 #[component]
-fn SimpleCounter(cx: Scope) -> impl IntoView {
-    let (count, set_count) = create_signal(cx, get_stored_count().unwrap_or_default());
+fn SimpleCounter() -> impl IntoView {
+    let (count, set_count) = create_signal(get_stored_count().unwrap_or_default());
 
     window_event_listener(ev::storage, move |_| {
         if let Some(stored_count) = get_stored_count() {
-            if stored_count != count() {
+            if stored_count != count.get() {
                 set_stored_count(stored_count);
-                set_count(stored_count)
+                set_count.set(stored_count)
             }
         }
     });
 
     let decrement = move |_| {
-        let result = count() - 1;
+        let result = count.get() - 1;
         set_stored_count(result);
-        set_count(result);
+        set_count.set(result);
     };
 
     let increment = move |_| {
-        let result = count() + 1;
+        let result = count.get() + 1;
         set_stored_count(result);
-        set_count(result);
+        set_count.set(result);
     };
 
-    view! { cx,
+    view! {
         <div class={"flex h-screen flex-col justify-center"}>
             <div id="count-layout">
                 {
-                    move || view! {cx, <div class={"text-slate-900 dark:text-white text-center text-9xl"}>{count()}</div>}
+                    move || view! {<div class={"text-slate-900 dark:text-white text-center text-9xl"}>{count.get()}</div>}
                 }
             </div>
             <div class={"add-subtract-buttons"}>
-                    {move || if count() == 0 {
-                        view!{cx, <div></div>}.into_any()
+                    {move || if count.get() == 0 {
+                        view!{<div></div>}.into_any()
 
                     } else {
-                        view! {cx,
+                        view! {
                             <button id="decrement-button" class={"text-slate-900 dark:text-white box-border h-32 w-32 p-4 border-2"} on:click={decrement}>
                                 <i class="fa-solid fa-minus"></i>
                             </button>
@@ -79,5 +79,5 @@ fn main() {
     console_error_panic_hook::set_once();
     _ = console_log::init_with_level(log::Level::Info);
     console_error_panic_hook::set_once();
-    mount_to_body(|cx| view! { cx,  <SimpleCounter/> })
+    mount_to_body(|| view! {  <SimpleCounter/> })
 }
